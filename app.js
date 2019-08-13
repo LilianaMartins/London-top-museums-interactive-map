7fb7f41d3f052c1f7ae65893e18578129ffd9eac
Liliana Martins <lilianasrmartins@gmail.com>
2019-08-13 23:13:49 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function nav_Open() {
+  document.getElementById('nav').className = "";
+  document.getElementById('navToggleOpen').className = "hidden"
+}
+function nav_Close () {
+  document.getElementById('nav').className = "hidden";
+  document.getElementById('navToggleOpen').className = " "
+
+}
